095cd5db4fe70ccfdab36cfc6fd57c7a71fc3bb1
marlonchiu <Knight1898@outlook.com>
2025-07-11 12:00:42 +0800

    4-13 useTransition 和 memo 的使用详解

--- lesson-advanced/src/index.js
+++ lesson-advanced/src/index.js
@@ -1,6 +1,6 @@
 import React, { Profiler } from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './lesson39/App'
+import App from './lesson40/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
---
+++ lesson-advanced/src/lesson40/App.jsx
@@ -0,0 +1,55 @@
+import { useState, memo } from 'react'
+
+// 组件所依赖的 props 没有发生变化，那么这个组件就不进行重新渲染，使用缓存
+const Child = memo(
+  ({ name, address }) => {
+    console.log('Child render')
+    return (
+      <div>
+        {name}
+        {address}
+      </div>
+    )
+  },
+  (originProps, props) => {
+    console.log('🚀 ~ originProps:', originProps)
+    console.log('🚀 ~ props:', props)
+    // 如果 address 发生变化，不用缓存
+    if (originProps.address !== props.address) {
+      return false
+    }
+    // 否则，使用缓存
+    return true
+  }
+)
+
+function App() {
+  const [name, setName] = useState('')
+  const [address, setAddress] = useState('')
+
+  return (
+    <>
+      <div>
+        name:{' '}
+        <input
+          value={name || ''}
+          onChange={(e) => {
+            setName(e.target.value)
+          }}
+        />
+      </div>
+      <div>
+        address:{' '}
+        <input
+          value={address || ''}
+          onChange={(e) => {
+            setAddress(e.target.value)
+          }}
+        />
+      </div>
+      <Child name={name} address={address}></Child>
+    </>
+  )
+}
+
+export default App
