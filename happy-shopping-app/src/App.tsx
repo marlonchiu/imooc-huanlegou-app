3e8df01205eafe5d48e84871b9dcd2808111eda8
marlonchiu <Knight1898@outlook.com>
2025-07-11 16:20:55 +0800

    feat: 项目搭建 一像素边框问题

---
+++ happy-shopping-app/src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <>
+      <div className="border-bottom" style={{ fontSize: '.2rem' }}>
+        欢乐购吧欢乐购吧欢乐购吧欢乐购吧欢乐
+      </div>
+    </>
+  )
+}
+export default App
